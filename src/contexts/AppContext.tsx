f88e17102d3ddffcf04a9b82c7504ec1fa14fe10
chado <chadon322@gmail.com>
2026-07-08 20:23:01 -0700

    Fix typescript warning

--- src/contexts/AppContext.tsx
+++ src/contexts/AppContext.tsx
@@ -125,7 +125,7 @@ export function AppProvider({ children }: { children: ReactNode }) {
     let cancelled = false
 
     getCurrentUser()
-      .then((res) => {
+      .then(() => {
         if (cancelled) return
       })
       .catch(async (err) => {
